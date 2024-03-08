4129acff39047a898b8db125b4d0ee25d3593108
Mohamed Chegri <mochegri@e2r5p3.1337.ma>
2024-03-08 04:19:39 +0100

    edit reckrute

--- scraping/scrapers/Reckrute.py
+++ scraping/scrapers/Reckrute.py
@@ -7,82 +7,36 @@ current_working_dir = './'
 logger = None
 
 def get_adresses(tag: BeautifulSoup) -> str:
-    """
-    Extracts the location from a BeautifulSoup tag and returns it after validation.
-
-    Args:
-        tag (BeautifulSoup): The BeautifulSoup tag containing job information.
-        logger (logging.Logger): Logger object for logging warning messages.
-
-    Returns:
-        str: The extracted location, or an empty string if not found or unable to extract.
-
-    Example:
-        location_tag = tag.find('div', class_='header-info').find('div', class_='location')
-        logger = set_logger()
-        location = get_locations(location_tag)
-        print(location)
-    """
     try:
-        print(tag)
-        location = tag.find('span', id='address')
-        print(location)
+        location = tag.find('span', id='address').text.strip()
+        logger.info(f"Adresses of the offer: {location}")
         return check_value_data(location, 'location', logger)
     except Exception as e:
         logger.warning(f"Location of the offer not found. Error: {e}")
         return missing_value
 
-def get_poste(offer_url: str) -> str:
-    """
-    Extracts the job title from a BeautifulSoup tag and returns it after validation.
-
-    Args:
-        tag (BeautifulSoup): The BeautifulSoup tag containing job information.
-        logger (logging.Logger): Logger object for logging warning messages.
-
-    Returns:
-        str: The extracted job title, or "non spécifié" if not found or unable to extract.
-
-    """
+def get_poste(tag: str) -> str:
     try:
-        match = re.search(r'offre-emploi-(.*?)-recrutement-(.*?)-(\d+).html', offer_url)
-        if match:
-            poste = match.group(1)
-            poste = poste.replace("-", " ")
-            logger.info(f"poste of the offer: {poste}")
-            return check_value_data(poste, 'poste', logger)
-        else:
-            logger.warning(f"poste name of the offer not found  No match found. in th URL. Error: {e}")
-            raise("No match found")
+        title_text = tag.find('title').string
+        job_title = title_text.split('-')[0].strip()
+        job_title = job_title.replace("Offre  d'emploi ", "")
+        logger.info(f"job title of the offer: {job_title}")
+        return check_value_data(job_title, 'job title', logger)
     except Exception as e:
-        logger.warning(f"poste name of the offer not found. Error: {e}")
-        return  
+        logger.warning(f"job title of the offer not found. Error: {e}")
+        return None
 
-def get_company(offer_url: str) -> str:
-    """
-    Extracts the company name from a BeautifulSoup tag and returns it after validation.
 
-    Args:
-        tag (BeautifulSoup): The BeautifulSoup tag containing job information.
-        logger (logging.Logger): Logger object for logging warning messages.
-
-    Returns:
-        str: The extracted company name, or an empty string if not found or unable to extract.
-
-    """
+def get_company(tag: BeautifulSoup) -> str:
     try:
-        match = re.search(r'recrutement-(.*?)-(\d+).html', offer_url)
-        if match:
-            company = match.group(1)
-            job_id = match.group(2)
-            logger.info(f"Company name of the offer {company} found in th URL {job_id}")
-            return check_value_data(company, 'company', logger)
-        else:
-            logger.warning(f"Company name of the offer not found  No match found. in th URL. Error: {e}")
-            raise("No match found")
+        title_tag = tag.find('title')
+        title_text = title_tag.string
+        company_name = title_text.split('-')[-1].strip()
+        logger.info(f"Company name of the offer: {company_name}")
+        return check_value_data(company_name, 'company', logger)
     except Exception as e:
         logger.warning(f"Company name of the offer not found. Error: {e}")
-        return missing_value
+        return None
 
 def get_contrat(tag: BeautifulSoup) -> str:
     """
@@ -186,25 +140,15 @@ def get_profile(tag: BeautifulSoup) -> str:
         return missing_value
 
 def get_missions(tag: BeautifulSoup) -> str:
-    """
-    Extracts the missions information from a BeautifulSoup tag and returns it after validation.
-
-    Args:
-        tag (BeautifulSoup): The BeautifulSoup tag containing job information.
-        logger (logging.Logger): Logger object for logging warning messages.
-
-    Returns:
-        str: The extracted missions information, or an empty string if not found or unable to extract.
-    """
     try:
-        heading_div = tag.find("h3", class_="heading", string="Poste à occuper :")
-        missions = heading_div.find_next_sibling("div").text.strip()
-        return check_value_data(missions, 'Missions :', logger)
-    except AttributeError:
-        logger.warning(f"Missions of the offer not found")
+        h2_tag = tag.find('h2', string='Poste :')
+        missions = h2_tag.parent.get_text(strip=True)
+        logger.info(f"Details de poste: {missions}")
+        return check_value_data(missions, 'Details de poste:', logger)
     except Exception as e:
-        logger.warning(f"Missions information cant scraped. Error: {e}")
-        return missing_value
+        logger.warning(f"Details de post of the offer not found. Error: {e}")
+        return None
+        
 
 def get_business_sector(tag: BeautifulSoup) -> str:
     """
@@ -323,27 +267,26 @@ def scrape_one_offer(offer_url: str) -> dict:
         dict: A dictionary containing various job offer information.
     """
     try:
-        response = requests.get(offer_url)
+        html_content = get_html_content(offer_url, logger)
+        logger.info(f"The server could get the offer source page succesfuly")
     except Exception as e:
-        logger.info(f"The server {offer_url} could not be found. Error: {e}")
-        return {}
+        logger.error(f"The server {offer_url} could not be found. Error: {e}")
+        return
 
-    html_content = response.content
+    # html_content = response.content
     soup = BeautifulSoup(html_content, 'html.parser')
-    # print(f"______{offer_url}_____________")
-    print(f"{soup}")
-    print(f"___________________")
+    # print(soup)
     return {
-        # "Societes": get_company(offer_url),
-        # "Postes": get_poste(offer_url),
-        # "Adresses": get_adresses(soup),
-        
+        # ,Societés,Postes,Adresses,Details de poste,Date de publication,Location,Postes proposés,Secteur d'activité,Fonctions,Expérience requise,Niveau d’étude demandé,Type de contrat proposé,Profil recherché,Decsription d'entreprise,Traits de personnalité
+        "Societés": get_company(soup),
+        "Postes": get_poste(soup),
+        "Adresses": get_adresses(soup),
+        "Details de poste" : get_missions(soup),
         # "Secteur d'activité": get_business_sector(soup),
         # "Locations": get_locations(soup),
         # "Dates": get_date(soup),
         # "Contrats": get_contrat(soup),
         # "Profils": get_profile(soup),
-        # "Missions": get_missions(soup),
         # "Details entreprises": get_recruiter(soup),
         # "Salaire": get_salary(soup),
         # "Metiers": get_job(soup),
@@ -354,32 +297,30 @@ def scrape_one_offer(offer_url: str) -> dict:
 
 def extract_offer_links(page_url):
     try:
-        logger.info(f"Start scraping data with the URL: {page_url}")
         html_content = get_html_content(page_url, logger)
-        # print(html_content)
-        if not html_content:
-            return None
-        logger.info(f"The page was fetched successfully from: {page_url}")
+        # logger.debug(f"html_content: {html_content}")÷
+        # if not html_content:
+        #     return None
+        logger.info(f"The page was fetched successfully")
     except Exception as e:
-        logger.warning(f"The server {page_url} could not be reached. Error: {e}")
+        logger.error(f"The server {page_url} could not be reached. Error: {e}")
         return None
     soup = BeautifulSoup(html_content, 'html.parser')
     try:
         titreJobs = soup.find_all('a', class_='titreJob')
-        logger.info(f"Extract {len(titreJobs)} titreJob")
+        logger.info(f"Extract {len(titreJobs)} titreJob from the page")
         if not titreJobs:
             raise Exception("There are no divs with a tag name [titreJob]. Cannot get offers from the page.")
     except Exception as e:
         logger.error(f"We could not fetch offers from this page {page_url}. Error: {e}")
         return None
     
-    logger.info(f"Start scraping {len(titreJobs)} offers on {page_url}")
     try:
         for offer_box in titreJobs:
             offer_url = f"https://www.rekrute.com{offer_box['href']}"
-        if not offer_url:
-            raise Exception("There is no URL in the offers section. Cannot get the offer link page.")
-        yield offer_url
+            if not offer_url:
+                raise Exception("There is no URL in the offers section. Cannot get the offer link page.")
+            yield offer_url
     except Exception as e:
         logger.error(f"Error while getting url offers: {e}")
     logger.info(f"Extract the link of an offer: {offer_url}")
@@ -404,15 +345,17 @@ def fetch_and_yield_offers(site_name):
     nbr_of_offer = 0
     try:
         for page_url in get_pages():
-            print(page_url)
+            logger.debug(f"start scraping the page: {page_url}")
             for url_offer in extract_offer_links(page_url):
-                print(url_offer)
+                logger.debug(f"start fitching data for this offer: {url_offer}")
                 # if not url_offer or already_scraperd(url_offer, site_name, logger):
                 #     return None
-                # offer_data = scrape_one_offer(url_offer)
-                # indexed_data = {"": nbr_of_offer}
-                # indexed_data.update(offer_data)
-                # yield indexed_data
+                offer_data = scrape_one_offer(url_offer)
+                print(f"offer_data: {offer_data}")
+                indexed_data = {"": nbr_of_offer}
+                indexed_data.update(offer_data)
+                yield indexed_data
+                exit(0)
                 nbr_of_offer += 1
     except Exception as e:
         logger.error(f"Error occurred in scraping pages: {e}")
@@ -424,17 +367,16 @@ def run_scraper(site_name, frequency, schema):
     df = spark.createDataFrame([], schema=schema)
     for offer_data in fetch_and_yield_offers(site_name):
         if offer_data:
-            print(offer_data)
-            # current_df = spark.createDataFrame([offer_data], schema=schema)
-            # df = df.union(current_df)
-    # if(df) not fid
-        # df.write.mode("overwrite").csv(f"../data/parquet/{frequency}/{site_name}/{site_name}parquet-{current_date_as_string()}.csv", header=True)
-        # pandas_df = df.toPandas()
-        # pandas_df.to_csv(f"../data/raw/{frequency}/{site_name}/{site_name}-{current_date_as_string()}.csv", index=False)
-    # else:
-        # generate an empty array
-    # df.show()
-    # spark.stop()
+            current_df = spark.createDataFrame([offer_data], schema=schema)
+            df = df.union(current_df)
+    if len(df):
+        df.write.mode("overwrite").csv(f"../data/parquet/{frequency}/{site_name}/{site_name}parquet-{current_date_as_string()}.csv", header=True)
+        pandas_df = df.toPandas()
+        pandas_df.to_csv(f"../data/raw/{frequency}/{site_name}/{site_name}-{current_date_as_string()}.csv", index=False)
+    # # else:
+    #     generate an empty array
+    df.show()
+    spark.stop()
     # return (calcu)
 
 # sef statistic()
@@ -463,18 +405,18 @@ def Reckrute_scraper(site_name: str, frequency: str = "daily"):
         StructField("Societés", StringType(), True),
         StructField("Postes", StringType(), True),
         StructField("Adresses", StringType(), True),
-        # StructField("Details de poste", StringType(), True),
-        # StructField("Date de publication", StringType(), True),
-        # StructField("Location", StringType(), True),
-        # StructField("Postes proposés", StringType(), True),
-        # StructField("Secteur d'activité", StringType(), True),
-        # StructField("Fonctions", StringType(), True),
-        # StructField("Expérience requise", StringType(), True),
-        # StructField("Niveau d’étude demandé", StringType(), True),
-        # StructField("Type de contrat proposé", StringType(), True),
-        # StructField("Profil recherché", StringType(), True),
-        # StructField("Decsription d'entreprise", StringType(), True),
-        # StructField("Traits de personnalité", StringType(), True),
+        StructField("Details de poste", StringType(), True),
+        StructField("Date de publication", StringType(), True),
+        StructField("Location", StringType(), True),
+        StructField("Postes proposés", StringType(), True),
+        StructField("Secteur d'activité", StringType(), True),
+        StructField("Fonctions", StringType(), True),
+        StructField("Expérience requise", StringType(), True),
+        StructField("Niveau d’étude demandé", StringType(), True),
+        StructField("Type de contrat proposé", StringType(), True),
+        StructField("Profil recherché", StringType(), True),
+        StructField("Decsription d'entreprise", StringType(), True),
+        StructField("Traits de personnalité", StringType(), True),
     ])
     try:
         # Set up logger
@@ -498,4 +440,10 @@ def main(frequency):
     Reckrute_scraper(web_site, frequency)
 
 if __name__ == '__main__':
-    main(frequency = 'daily')
+    # main(frequency = 'daily')
+    logger = set_logger("rekrute", "dayly")
+    
+    offer_url = "https://www.rekrute.com/offre-emploi-technicien-maintenance-automaticien-senior-recrutement-crouzet-casablanca-159053.html"
+    # offer_url = "https://www.rekrute.com/offre-emploi-technicien-maintenance-automaticien-senior-recrutement-crouzet-casablanca-159053.html"
+    s = scrape_one_offer(offer_url)
+    print(s)
